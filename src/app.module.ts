4748f2aaeb42c3018c472a32856f4bec4d4c4cd5
amirali729 <amirpechs10@gmail.com>
2026-08-22 22:03:53 +0500

    feat: implement authentication module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/controller/auth.controller';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [],
+  controllers: [AuthController],
   providers: [],
 })
 export class AppModule {}
