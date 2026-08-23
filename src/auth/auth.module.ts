12c8ce5578cd162e93ac0cfafc2f70f6df99bb18
amirali729 <amirpechs10@gmail.com>
2026-08-23 21:26:28 +0500

    feat: update AuthModule to include AuthRepository in providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
+import { AuthRepsoitory } from './repository/auth.repository';
 import { AuthController } from './controller/auth.controller';
 
 @Module({
-  imports: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthRepsoitory],
+  controllers: [AuthController],
 })
 export class AuthModule {}
